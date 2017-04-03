e8b5bd1456629acfceec41c78ea036aa1fe237b6
Jaeho Lee <owen12123@gmail.com>
2017-04-03 18:01:12 -0400

    train and main function

---
+++ train.py
@@ -0,0 +1,10 @@
+import cv2
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import csv
+
+#Jaeho's image path
+#path = '/Users/lordent/Desktop/X_Train'
+#Ray's image path
+#path = ''
